479ead9bb24d096ec3c53231ae235d390faa3bed
user140592 <user140592@eu201.local>
2019-02-04 20:52:49 +0000

    add path

--- StreamAnimation/ViewController.swift
+++ StreamAnimation/ViewController.swift
@@ -12,9 +12,52 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let curvedView = CurvedView(frame: view.frame)
+        curvedView.backgroundColor = .yellow
+        
+        view.addSubview(curvedView)
+
+        let imageView = UIImageView(image: UIImage(named: "like-2"))
+        imageView.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+       
+        let animation = CAKeyframeAnimation(keyPath: "position")
+        
+        animation.path = customPath().cgPath
+        animation.duration = 2
+        animation.fillMode = CAMediaTimingFillMode.forwards
+        animation.isRemovedOnCompletion = false
+        animation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut) 
+        
+        imageView.layer.add(animation, forKey: nil)
+        view.addSubview(imageView)
+        
     }
 
 
+}
+
+func customPath() -> UIBezierPath {
+    let path = UIBezierPath()
+    
+    path.move(to: CGPoint(x: 0, y: 200))
+    
+    let endPath = CGPoint(x: 400, y: 200)
+    
+    let cp1 = CGPoint(x: 100, y: 200)
+    let cp2 = CGPoint(x: 200, y: 300)
+    path.addCurve(to: endPath, controlPoint1: cp1, controlPoint2: cp2)
+    return path
+}
+
+class CurvedView: UIView {
+    
+    override func draw(_ rect: CGRect) {
+        
+        let path = customPath()
+        
+        path.lineWidth = 3
+        path.stroke()
+    }
 }
 
